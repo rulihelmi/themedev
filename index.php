5f64fcfa24d0b3be6d25504dd53ddb851e71bf68
Ruli Helmi <rulinizar@gmail.com>
2021-03-19 13:46:44 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,18 @@
+<<?php
+/**
+* main template file.
+*
+* @package Aquila
+*/
+
+ ?>
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WordPress Theme</title>
+  </head>
+  <body>
+    Hello WordPress
+  </body>
+</html>
